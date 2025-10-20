8294cbbd955fbc0b29be619087d648647d504a18
suzhanfeng <suzhanfeng@taqu.cn>
2025-10-20 22:00:23 +0800

    feat(ios): initialize multi-module services in iOS app entry

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
+
+    init() {
+      //用于初始化多模块的页面服务
+      PageInitializer.shared.initializePages()
+    }
+
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
